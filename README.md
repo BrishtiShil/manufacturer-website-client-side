9539d07f90902e46efdb1303e5d9e83dff68b487
Brishti Shil <brishtishil62@gmail.com>
2022-05-25 20:25:29 +0600

    login page create

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# Getting Started with Create React App
+# Menages Tools Manufacturer Website
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Available Scripts
+## Available tec
 
-In the project directory, you can run:
+
 
 ### `npm start`
 
--- src/Pages/Tools/Tools.js
+++ src/Pages/Tools/Tools.js
@@ -11,7 +11,7 @@ const Tools = () => {
     }, [])
     return (
         <div>
-            <h2 className='text-4xl text-bold text-primary text-center my-12'>Available Tools</h2>
+            <h2 className='text-4xl font-bold text-center my-12'>Available Tools</h2>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 '>
                 {
                     services.map(service => <Service
