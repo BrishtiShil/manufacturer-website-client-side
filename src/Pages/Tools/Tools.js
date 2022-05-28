8a51a01bff8d26f8a5ba53f15a0d9d0dc9ef40ca
Brishti Shil <brishtishil62@gmail.com>
2022-05-28 16:39:07 +0600

    data load from heroku

--- src/Pages/Tools/Tools.js
+++ src/Pages/Tools/Tools.js
@@ -7,7 +7,7 @@ const Tools = () => {
     const [product, setProduct] = useState(null);
 
     useEffect(() => {
-        fetch('http://localhost:5000/service')
+        fetch('https://boiling-badlands-02194.herokuapp.com/service')
             .then(res => res.json())
             .then(data => setServices(data));
     }, [])
